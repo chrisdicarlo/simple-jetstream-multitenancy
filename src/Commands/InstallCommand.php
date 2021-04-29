b5fe6fe9b985e61ff6c8106ea199a9746fd3090c
Chris Di Carlo <chris@dicarlosystems.ca>
2021-04-29 14:14:03 -0400

    Install command wip

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -16,6 +16,29 @@ class InstallCommand extends Command
             $this->callSilent('vendor:publish', ['--tag' => 'simple-jetstream-multitenancy-config', '--force' => true]);
         }
 
+        $tenantModel = $this->choice(
+            'Which model will be your tenant?',
+            ['App\Models\Team', 'App\Models\User'],
+            0,
+            $maxAttempts = null,
+            $allowMultipleSelections = false
+        );
+
+        $this->replaceInFile("'tenant_model' => null", "'tenant_model' => '{$tenantModel}'", config_path('simple-jetstream-multitenancy.php'));
+
         $this->comment('All done');
+    }
+
+    /**
+     * Replace a given string within a given file.
+     *
+     * @param  string  $search
+     * @param  string  $replace
+     * @param  string  $path
+     * @return void
+     */
+    protected function replaceInFile($search, $replace, $path)
+    {
+        file_put_contents($path, str_replace($search, $replace, file_get_contents($path)));
     }
 }
---
+++ src/Models/Concerns/TenantAware.php
@@ -0,0 +1,31 @@
+<?php
+namespace Chrisdicarlo\SimpleJetstreamMultitenancy\Models\Concerns;
+
+trait TenantAware
+{
+    public static function bootTenantAware() {
+        if (auth()->check()) {
+
+            $tenantModel = config('simple-jetstream-multitenancy.tenant_model');
+            
+            static::creating(function ($model) use ($tenantModel) {
+                if($tenantModel == 'App\Models\Team') {
+                    $model = auth()->user()->currentTeam()->id;
+                } else {
+                    $model->tenant_id = auth()->id();
+                }
+            });
+
+            static::addGlobalScope('tenant_id', function (Builder $builder) use ($tenantModel) {
+                if (auth()->check()) {
+                    if($tenantModel == 'App\Models\Team') {
+                        return $builder->where('tenant_id', auth()->user()->currentTeam()->id);
+                    }
+                    
+                    return $builder->where('tenant_id', auth()->id());
+                    
+                }
+            });
+        }
+    }
+}
