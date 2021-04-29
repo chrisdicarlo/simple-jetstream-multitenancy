43da4575218a15b02bfdeb9bc613ab4972497884
chrisdicarlo <chrisdicarlo@users.noreply.github.com>
2021-04-29 17:50:16 +0000

    Fix styling

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -12,7 +12,7 @@ class InstallCommand extends Command
 
     public function handle()
     {
-        if(! file_exists(config_path('simple-jetstream-multitenancy.php'))) {
+        if (! file_exists(config_path('simple-jetstream-multitenancy.php'))) {
             $this->call('vendor:publish --tag=simple-jetstream-multitenancy-config');
         }
         
